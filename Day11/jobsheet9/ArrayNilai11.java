eff51e7d3318e8185a7818f8724ab3f9f9136afa
helmi <137972846+teathricalhood122@users.noreply.github.com>
2025-11-05 21:23:29 +0700

    Menambahkan Array Nilai percobaan 2

---
+++ Day11/jobsheet9/ArrayNilai11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukan Nilai Akhir ke- " + i + ": ");
+            nilaiAkhir[i] = sc.nextInt();  
+        }
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " Lulus!");
+            } else {
+                System.out.println("Mahasiswa ke- " + i + " Tidak Lulus!");
+            }
+        }
+    }
+}
