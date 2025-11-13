25a2c1bc948cb14e4659e486db05e9eee8073422
helmi <137972846+teathricalhood122@users.noreply.github.com>
2025-11-13 15:30:23 +0700

    Tugas 2

--- Day12/jobsheet10/tugasSurvey2.java
+++ Day12/jobsheet10/tugasSurvey2.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 
-public class tugasSurvey1 {
+public class tugasSurvey2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int survey[][] = new int[10][6];
+
+        int survey[][] = new int[3][6];
+        double rata2;
+        double total;
 
         System.out.println("Input Data Survey Kepuasan Pelanggan");
         System.out.println();
 
         for (int i = 0; i < survey.length; i++) {
-            System.out.println("Responden ke- " + (i+1));
+            total = 0;
+            System.out.println("Responden ke-" + (i+1));
 
             for (int j = 0; j < survey[i].length; j++) {
-                System.out.println("Pertanyaan ke- " + (j+1) + ": ");
+                System.out.println("Pertanyaan ke-" + (j+1));
                 survey[i][j] = sc.nextInt();
+                total += survey[i][j];
             }
+
+            rata2 = total / survey[i].length;
+            System.out.println("Rata-rata responden ke-" + (i+1) + "adalah: " + rata2 ) ;
             System.out.println();
         }
     }
